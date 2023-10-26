d957832885c7dda6bde0a314a2062ff0e7547224
marinajcs <marinacarranza163@gmail.com>
2023-10-26 10:56:43 +0200

    Creada entidad plan de rodaje closes #12

---
+++ src/planRodaje.go
@@ -0,0 +1,10 @@
+package src
+
+// PlanRodaje representa un plan de rodaje que contiene una serie de planos
+type PlanRodaje struct {
+	codigo     string  // Código único del plan de rodaje
+	planos     []Plano // Lista de planos que forman parte del plan de rodaje
+	pelicula   string  // Nombre de la película a la que se le asigna el plan
+	direccion  string  // Nombre del director o directora
+	produccion string  // Nombre del productor o productora
+}
