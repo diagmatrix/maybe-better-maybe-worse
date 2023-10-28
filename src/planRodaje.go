33fe0b71a31cf7a82d9920a6275b2c117680ace2
Marina Jun Carranza Sánchez <104431116+marinajcs@users.noreply.github.com>
2023-10-28 12:40:53 +0200

    Modificadas variables de clase en planRodaje.go
    Las variables dirección y producción se han convertido en un slice de string, ya que puede ser un plan de rodaje dirigido/producido por más de una persona.

--- src/planRodaje.go
+++ src/planRodaje.go
@@ -5,8 +5,8 @@ type PlanRodaje struct {
 	codigo     string  // Código único del plan de rodaje
 	planos     []Plano // Lista de planos que forman parte del plan de rodaje
 	pelicula   string  // Nombre de la película a la que se le asigna el plan
-	direccion  string  // Nombre del director o directora
-	produccion string  // Nombre del productor o productora
+	direccion  []string  // Nombre del director o directora
+	produccion []string  // Nombre del productor o productora
 }
 
 // NewPlanRodaje crea una nueva instancia de la estructura PlanRodaje con los datos proporcionados y la devuelve como un puntero
