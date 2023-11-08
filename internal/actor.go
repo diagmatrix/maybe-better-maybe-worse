63d0792fe0e2ab42dabde8a59610de23d5b3017a
marinajcs <marinacarranza163@gmail.com>
2023-11-08 19:47:29 +0100

    Eliminados constructores que no añaden a los structs de actor, plano y planrodaje
    Simplemente inicializaban los valores que se le pasaban por parámetros y no incluían comportamientos adicionales

--- internal/actor.go
+++ internal/actor.go
@@ -10,28 +10,3 @@ type Actor struct {
 	planosARodar   []Plano     // Planos en los que participa
 }
 
-// NewActor crea una nueva instancia de la estructura Actor con los datos proporcionados y la devuelve como un puntero
-//
-// Parámetros:
-//
-//	dni (string): el número de identificación del actor
-//	nombre (string): el nombre del actor
-//	edad (int): la edad del actor
-//	disponibilidad ([]time.Time): un slice de objetos time.Time que representa los horarios de disponibilidad del actor
-//
-// Valor de retorno:
-//
-//	*Actor: un puntero a la nueva instancia de Actor creada con los datos proporcionados
-//
-// Ejemplo de uso:
-//
-//	disponibilidad := []time.Time{time.Date(2023, time.October, 26, 9, 0, 0, 0, time.UTC), time.Date(2023, time.October, 27, 14, 30, 0, 0, time.UTC),}
-//	actor := NewActor("123456789", "Juana Jiménez", 30, disponibilidad)
-func NewActor(DNI, nombre string, edad int, disponibilidad []time.Time) *Actor {
-	return &Actor{
-		dni:            DNI,
-		nombre:         nombre,
-		edad:           edad,
-		disponibilidad: disponibilidad,
-	}
-}
