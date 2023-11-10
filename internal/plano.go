cbc9a003170e508d5430a53578ee611d0665a176
Marina Jun Carranza Sánchez <104431116+marinajcs@users.noreply.github.com>
2023-11-10 09:44:18 +0100

    Eliminados comentarios triviales en documentación de plano, fixes #13

--- internal/plano.go
+++ internal/plano.go
@@ -4,9 +4,9 @@ import "time"
 
 // Plano representa un plano de rodaje con los detalles más relevantes
 type Plano struct {
-	tiempoEstimado time.Duration // Tiempo estimado de grabación del plano
-	actores        []Actor       // Lista de actores que participarán en el plano
-	lugar          string        // Lugar donde se llevará a cabo el rodaje del plano
-	fechaInicio    time.Time     // Fecha en la que se rodará el plano
+	tiempoEstimado time.Duration
+	actores        []Actor
+	lugarGrabacion string
+	fechaInicio    time.Time
 }
 
