496d76b86af5be18a585291605c979c0c9b41294
Marina Jun Carranza Sánchez <104431116+marinajcs@users.noreply.github.com>
2023-11-10 10:05:42 +0100

    Corregido formato de struct Actor, fixes #14
    El comentario no se correspondía a lo incluido explícitamente en el código y había un pequeño error en el espaciado entre variable y tipo

--- internal/actor.go
+++ internal/actor.go
@@ -2,9 +2,8 @@ package internal
 
 import "time"
 
-// Actor representa un actor con información básica y su disponibilidad horaria.
 type Actor struct {
-	horariosDisponibilidad []time.Time
-	planosARodar   []Plano
+	horariosDisponibilidad 	[]time.Time
+	planosARodar   		[]Plano
 }
 
