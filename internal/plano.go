28a98ca6ee6c9e087e51a9b8d6030456073ca11b
Marina Jun Carranza Sánchez <104431116+marinajcs@users.noreply.github.com>
2023-11-10 10:07:56 +0100

    Eliminado comentario de struct Plano, fixes #13
    Los nombres del struct y sus variables son suficientes para documentar la propia entidad

--- internal/plano.go
+++ internal/plano.go
@@ -2,7 +2,6 @@ package internal
 
 import "time"
 
-// Plano representa un plano de rodaje con los detalles más relevantes
 type Plano struct {
 	tiempoEstimado time.Duration
 	actores        []Actor
