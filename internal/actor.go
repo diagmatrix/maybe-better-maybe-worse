15b13d4dd268b733987cb852721eb3e323b08e4f
Marina Jun Carranza Sánchez <104431116+marinajcs@users.noreply.github.com>
2023-11-10 09:41:55 +0100

    Eliminadas variables de actor no imprescindibles, fixes #14
    Se han suprimido las variables de nombre y DNI no necesarias para la implementación de la lógica de negocio. Adicionalmente, se han eliminado comentarios triviales.

--- internal/actor.go
+++ internal/actor.go
@@ -4,9 +4,7 @@ import "time"
 
 // Actor representa un actor con información básica y su disponibilidad horaria.
 type Actor struct {
-	dni            string      // Número de identificación del actor
-	nombre         string      // Nombre del actor
-	disponibilidad []time.Time // Horarios de disponibilidad del actor
-	planosARodar   []Plano     // Planos en los que participa
+	horariosDisponibilidad []time.Time
+	planosARodar   []Plano
 }
 
