d61972c523836793bdd2fa4a3fcf01172307c4df
Marina Jun Carranza Sánchez <104431116+marinajcs@users.noreply.github.com>
2023-11-10 09:57:25 +0100

    Suprimidas variables no imprescindibles en plan de rodaje, fixes #12
    Se han eliminado las variables: código, película, dirección y producción, ya que no son necesarias para la lógica de negocio ni se piden en las historias de usuario

--- internal/planrodaje.go
+++ internal/planrodaje.go
@@ -1,11 +1,6 @@
 package internal
 
-// PlanRodaje representa un plan de rodaje que contiene una serie de planos
 type PlanRodaje struct {
-	codigo     string  // Código único del plan de rodaje
-	planos     []Plano // Lista de planos que forman parte del plan de rodaje
-	pelicula   string  // Nombre de la película a la que se le asigna el plan
-	direccion  []string  // Nombre del director o directora
-	produccion []string  // Nombre del productor o productora
+	planos     []Plano
 }
 
